36c21ddf0ba9b9a6bc22a0d0cec5a6c68e5ba57c
yghtasdeasfgsd <yghtasdeasfgsd@CSONGOR>
2026-01-30 19:19:13 +0100

    Fixed merge

--- Év végi projekt/C#/BarberManager/BarberManager/ViewModels/MainWindowViewModel.cs
+++ Év végi projekt/C#/BarberManager/BarberManager/ViewModels/MainWindowViewModel.cs
@@ -5,48 +5,40 @@ namespace BarberManager.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    private DateTime _currentMonday;
-    private int _weekShift;
+    // jelenlegi oldal
+    [ObservableProperty]
+    private ViewModelBase _currentPage;
 
-    public DateTime CurrentMonday
-    {
-        get => _currentMonday;
-        set
-        {
-            _currentMonday = value;
-            OnPropertyChanged(nameof(Mon));
-            OnPropertyChanged(nameof(Tue));
-            OnPropertyChanged(nameof(Wed));
-            OnPropertyChanged(nameof(Thu));
-            OnPropertyChanged(nameof(Fri));
-            OnPropertyChanged(nameof(Sat));
-            OnPropertyChanged(nameof(Sun));
-        }
-    }
-
-    public string Mon => CurrentMonday.ToString("MM.dd");
-    public string Tue => CurrentMonday.AddDays(1).ToString("MM.dd");
-    public string Wed => CurrentMonday.AddDays(2).ToString("MM.dd");
-    public string Thu => CurrentMonday.AddDays(3).ToString("MM.dd");
-    public string Fri => CurrentMonday.AddDays(4).ToString("MM.dd");
-    public string Sat => CurrentMonday.AddDays(5).ToString("MM.dd");
-    public string Sun => CurrentMonday.AddDays(6).ToString("MM.dd");
-
-    public ICommand NextWeek { get; }
-    public ICommand PrevWeek { get; }
+    [ObservableProperty]
+    private bool _isLoggedIn;
 
     public MainWindowViewModel()
     {
-        CurrentMonday = GetMonday(DateTime.Today);
+        // Kijelentkezve indul
+        IsLoggedIn = false;
+        ShowLoginScreen();
+    }
 
-        NextWeek = new RelayCommand(() =>
+    private void ShowLoginScreen()
+    {
+        var loginVm = new LoginViewModel();
+
+        // ha sikeres login
+        loginVm.OnLoginSuccess = () =>
         {
-            if (_weekShift < 4)
-            {
-                _weekShift++;
-                CurrentMonday = CurrentMonday.AddDays(7);
-            }
-        });
+            IsLoggedIn = true;
+            CurrentPage = new AppointmentsViewModel();
+        };
+
+        CurrentPage = loginVm;
+    }
+
+    [RelayCommand]
+    public void Logout()
+    {
+        IsLoggedIn = false;
+        ShowLoginScreen();
+    }
 
     [RelayCommand]
     public void NavigateToAppointments()
