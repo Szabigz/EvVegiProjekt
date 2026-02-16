795c149637d1cf343e09fcbc2c99955c517742f0
yghtasdeasfgsd <yghtasdeasfgsd@CSONGOR>
2026-02-16 17:13:05 +0100

    Added ApiService class

---
+++ Év végi projekt/C#/BarberManager/BarberManager/Services/ApiService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using BarberManager.Models;
+
+namespace BarberManager.Services
+{
+    public class ApiService
+    {
+        // Beallitasok
+        private const string BaseUrl = "http://localhost:3000";
+
+        private readonly HttpClient _httpClient;
+        private string _jwtToken = string.Empty;
+
+        public ApiService()
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(BaseUrl)
+            };
+        }
+
+
+
+    }
+}
