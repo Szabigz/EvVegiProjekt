e1b9882a560bd3d56772dda523d7d14a4326241a
yghtasdeasfgsd <yghtasdeasfgsd@CSONGOR>
2026-02-16 16:52:50 +0100

    Added Appointment class

---
+++ Év végi projekt/C#/BarberManager/BarberManager/Models/Appointment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace BarberManager.Models
+{
+    public class Appointment
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("userID")]
+        public int UserId { get; set; }
+
+        [JsonPropertyName("barberID")]
+        public int BarberId { get; set; }
+
+        [JsonPropertyName("serviceID")]
+        public int ServiceId { get; set; }
+
+        [JsonPropertyName("start_time")]
+        public DateTime StartTime { get; set; }
+
+        [JsonPropertyName("end_time")]
+        public DateTime EndTime { get; set; }
+
+        [JsonPropertyName("status")]
+        public string Status { get; set; } = string.Empty; // "booked" "canceled" stb
+
+        [JsonPropertyName("comment")]
+        public string? Comment { get; set; }
+    }
+}
